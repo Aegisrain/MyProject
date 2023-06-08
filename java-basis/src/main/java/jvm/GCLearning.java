96a5804818e3e9a6054a5853d912038ea66526a3
yezhongyu <yezhongyu@sobey.com>
2023-06-08 16:29:04 +0800

    java基础学习

---
+++ java-basis/src/main/java/jvm/GCLearning.java
@@ -0,0 +1,32 @@
+package jvm;
+
+/**
+ * @Author yezhongyu
+ * @Date 2023年06月08日 15:28
+ * @Description 测试GC 分析日志
+ */
+public class GCLearning {
+    private static final int _1MB = 1024 * 1024;
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     */
+    public static void testAllocation() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB]; // 出现一次Minor GC
+    }
+
+    /**
+     * 大对象直接进入老年代
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     * -XX:PretenureSizeThreshold=3145728
+     */
+    public static void testPretenureSizeThreshold() {
+        byte[] allocation;
+        allocation = new byte[4 * _1MB]; //直接分配在老年代中
+    }
+
+}
